b9343f07fd9bd5ab5d9b7a20f82e789b85455dde
nizham romadhoni <nizhambq@gmail.com>
2025-02-11 11:35:32 +0700

    update main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int ledmerah = 8;
+const int ledkuning = 9;
+const int ledhijau = 10;
+
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+pinMode(ledmerah, OUTPUT);
+pinMode(ledkuning, OUTPUT);
+pinMode(ledhijau, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+digitalWrite(ledmerah, HIGH);
+delay(5000);
+digitalWrite(ledmerah, LOW);
+
+
+digitalWrite(ledkuning, HIGH);
+delay(1000);
+digitalWrite(ledkuning, LOW);
+
+
+digitalWrite(ledhijau, HIGH);
+delay(5000);
+digitalWrite(ledhijau, LOW);
+
+digitalWrite(ledkuning, HIGH);
+delay(1000);
+digitalWrite(ledkuning, LOW);
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
+
